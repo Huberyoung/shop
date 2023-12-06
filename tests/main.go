d3f7028a818be1f3c7d8452798be41ab53735840
huberyyang <yangcai@4399inc.com>
2023-12-06 21:52:25 +0800

    feat: shop

--- tests/main.go
+++ tests/main.go
@@ -6,13 +6,12 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	pUser "shop_srvs/proto/user"
-	"time"
 )
 
 var userClient pUser.UserClient
 var conn *grpc.ClientConn
 
-func init() {
+func Init() {
 	var err error
 	conn, err = grpc.Dial("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -20,21 +19,47 @@ func init() {
 	}
 	userClient = pUser.NewUserClient(conn)
 }
-func main() {
-	defer conn.Close()
-	for i := 5; i < 7; i++ {
-		user, err := userClient.CreateUser(context.Background(), &pUser.CreateUserRequest{
-			Nickname: fmt.Sprintf("喵喵%d", i),
-			Password: "123456",
-			Mobile:   fmt.Sprintf("1815956737%d", i),
-			Gender:   pUser.CreateUserRequest_MALE.Enum(),
-			BirthDay: uint64(time.Now().Unix()),
-		})
-		if err != nil {
-			fmt.Printf("userClient.CreateUser err:%s\n\n", err)
-		} else {
-			fmt.Printf("user:%+v\n", user)
-		}
-		time.Sleep(1 * time.Second)
+
+func TestGetUserList() {
+	rsp, err := userClient.GetUserList(context.Background(), &pUser.PageInfo{PageNum: 1, PageSize: 2})
+	if err != nil {
+		panic(err)
 	}
+
+	for _, user := range rsp.Data {
+		fmt.Printf("id:%d  gender:%d  mobile:%s  password:%s", user.Id, user.Gender, user.Mobile, user.Password)
+
+		rsp1, err1 := userClient.CheckPassword(context.Background(), &pUser.PasswordCheckRequest{
+			Password:          "123456",
+			EncryptedPassword: user.Password,
+		})
+		if err1 != nil {
+			panic(err1)
+		}
+		fmt.Println(rsp1.Success)
+
+	}
+}
+
+func main() {
+	Init()
+	defer conn.Close()
+	TestGetUserList()
+
+	//
+	//for i := 5; i < 7; i++ {
+	//	user, err := userClient.CreateUser(context.Background(), &pUser.CreateUserRequest{
+	//		Nickname: fmt.Sprintf("喵喵%d", i),
+	//		Password: "123456",
+	//		Mobile:   fmt.Sprintf("1815956737%d", i),
+	//		Gender:   pUser.CreateUserRequest_MALE.Enum(),
+	//		BirthDay: uint64(time.Now().Unix()),
+	//	})
+	//	if err != nil {
+	//		fmt.Printf("userClient.CreateUser err:%s\n\n", err)
+	//	} else {
+	//		fmt.Printf("user:%+v\n", user)
+	//	}
+	//	time.Sleep(1 * time.Second)
+	//}
 }
