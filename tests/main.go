ff9f37b8500c8a5ae4adb4c0cd79693e67e013b7
huberyyang <yangcai@4399inc.com>
2023-12-12 23:20:21 +0800

    feat:修改接口号

--- tests/main.go
+++ tests/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
 	pUser "shop_srvs/proto/user"
 )
 
@@ -13,7 +16,7 @@ var conn *grpc.ClientConn
 
 func Init() {
 	var err error
-	conn, err = grpc.Dial("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err = grpc.Dial("127.0.0.1:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
 	}
@@ -60,23 +63,23 @@ func TestGetUserList() {
 func main() {
 	Init()
 	defer conn.Close()
-	TestGetUserList()
-	testGetUserById()
-	testGetUserByMobile()
+	//TestGetUserList()
+	//testGetUserById()
+	//testGetUserByMobile()
 
-	//for i := 5; i < 7; i++ {
-	//	user, err := userClient.CreateUser(context.Background(), &pUser.CreateUserRequest{
-	//		Nickname: fmt.Sprintf("喵喵%d", i),
-	//		Password: "123456",
-	//		Mobile:   fmt.Sprintf("1815956737%d", i),
-	//		Gender:   pUser.CreateUserRequest_MALE.Enum(),
-	//		BirthDay: uint64(time.Now().Unix()),
-	//	})
-	//	if err != nil {
-	//		fmt.Printf("userClient.CreateUser err:%s\n\n", err)
-	//	} else {
-	//		fmt.Printf("user:%+v\n", user)
-	//	}
-	//	time.Sleep(1 * time.Second)
-	//}
+	for i := 1; i < 10; i++ {
+		user, err := userClient.CreateUser(context.Background(), &pUser.CreateUserRequest{
+			Nickname: fmt.Sprintf("喵喵%d", i),
+			Password: "123456",
+			Mobile:   fmt.Sprintf("1815956737%d", i),
+			Gender:   pUser.CreateUserRequest_MALE.Enum(),
+			BirthDay: uint64(time.Now().Unix()),
+		})
+		if err != nil {
+			fmt.Printf("userClient.CreateUser err:%s\n\n", err)
+		} else {
+			fmt.Printf("user:%+v\n", user)
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
